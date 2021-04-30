dbe240503d05321c1527567f8706067f11983fe5
Jhon <jhon.charryserrato@gmail.com>
2021-04-30 00:25:23 -0500

    First commit

---
+++ src/database/database.service.ts
@@ -0,0 +1,26 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConnectionOptions } from 'typeorm';
+import { ConfigModule } from '../config/config.module';
+import { ConfigService } from '../config/config.service';
+import { Configuration } from '../config/config.keys';
+
+
+export const databaseProviders = [
+    TypeOrmModule.forRootAsync({
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        async useFactory(config: ConfigService) {
+            return {
+                /*  ssl: true, */
+                type: 'postgres' as 'postgres',
+                host: config.get(Configuration.HOST),
+                username: config.get(Configuration.USERNAME),
+                password: config.get(Configuration.PASSWORD),
+                database: config.get(Configuration.DATABASE),
+                entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+                migrations: [__dirname + '/migrations/*{.ts,.js}'],
+                synchronize: true
+            } as ConnectionOptions
+        }
+    })
+]
