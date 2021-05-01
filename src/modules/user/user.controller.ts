e2234840ea1c91e769411256ef45629ba26693be
Jhon <jhon.charryserrato@gmail.com>
2021-05-01 15:15:18 -0500

    Added RoleGuard

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -3,12 +3,16 @@ import { UserDto } from './dto/user.dto';
 import { User } from './user.entity';
 import { UserService } from './user.service';
 import { AuthGuard } from '@nestjs/passport';
+import { Roles } from '../role/decorators/role.decorator';
+import { RoleGuard } from '../role/guards/role.guard';
 
 @Controller('users')
 export class UserController {
     constructor(private readonly _userService: UserService) { }
 
     @Get('/:id')
+    @Roles('ADMIN', 'AUTHOR')
+    @UseGuards(AuthGuard(), RoleGuard)
     async getUser(@Param('id', ParseIntPipe) id: number): Promise<User> {
         const user = await this._userService.get(id);
         return user;
